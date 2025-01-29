97866fa15f37c1f8aded04205b2b5a98ee53db98
DiegoDuaS <diego.slowing@gmail.com>
2025-01-29 17:50:21 -0600

    feat: import data file

--- analisis.py
+++ analisis.py
@@ -12,4 +12,11 @@ import scipy.stats as stats
 import statsmodels.stats.diagnostic as diag
 import statsmodels.api as sm
 
+#Configuracion para mostrar todas las filas y columnas
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
+#Importar Datos
+movies=pd.read_csv("movies.csv")
+
+movies.describe()
