fd42f85686350dee5f8442a0e451cb9944e95cd8
DiegoDuaS <diego.slowing@gmail.com>
2025-01-29 17:04:01 -0600

    feat: added imports and data file

---
+++ analisis.py
@@ -0,0 +1,15 @@
+""""
+Hoja de Trabajo 1 
+Mineria de Datos 
+Diego Duarte 22075
+José Marchena 22
+"""
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import statsmodels.stats.diagnostic as diag
+import statsmodels.api as sm
+
+
