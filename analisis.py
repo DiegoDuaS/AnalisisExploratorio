4c0753f372386cf48aba89d4410784b837283e76
DiegoDuaS <diego.slowing@gmail.com>
2025-01-29 18:18:25 -0600

    feat: information for description and types of the data

--- analisis.py
+++ analisis.py
@@ -11,12 +11,16 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import statsmodels.stats.diagnostic as diag
 import statsmodels.api as sm
+from tabulate import tabulate
 
 #Configuracion para mostrar todas las filas y columnas
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 #Importar Datos
-movies=pd.read_csv("movies.csv")
+movies=pd.read_csv("movies.csv", encoding="ISO-8859-1")
 
-movies.describe()
+#Resumen del Conjunto de Datos
+print(movies.describe())
+
+print(movies.info())
