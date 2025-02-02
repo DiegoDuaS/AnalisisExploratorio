edcf106f56dbfecc07715b2441dc244959ac75ab
DiegoDuaS <diego.slowing@gmail.com>
2025-02-02 14:29:25 -0600

    feat: added histograms to pruebas de normalidad

--- analisis.py
+++ analisis.py
@@ -25,6 +25,22 @@ def prueba_de_normalidad(column, column_name):
     else:
         print(f"No se rechaza la hipótesis nula: los datos de '{column_name}' parecen provenir de una distribución normal." + "\n")
         
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+
+    # Graficar histograma
+    axes[0].hist(column)
+    axes[0].set_title(f'Histograma de {column_name}')
+    axes[0].set_xlabel(column_name)
+    axes[0].set_ylabel('Densidad')
+
+    # Graficar boxplot
+    axes[1].boxplot(column)
+    axes[1].set_title(f'Boxplot de {column_name}')
+    axes[1].set_ylabel(column_name)
+    # Mostrar gráficos
+    plt.tight_layout()
+    plt.show()
+        
 def frecuencias(column):
     frecuencia = column.value_counts()
     porcentaje = column.value_counts(normalize=True) * 100
