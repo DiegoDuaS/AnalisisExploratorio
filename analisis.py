40ae9647afe10fc48bc1e5db956d6cb5c4049166
DiegoDuaS <diego.slowing@gmail.com>
2025-02-02 15:49:25 -0600

    feat: added functions for key questions

--- analisis.py
+++ analisis.py
@@ -13,12 +13,14 @@ import statsmodels.stats.diagnostic as diag
 import statsmodels.api as sm
 
 def prueba_de_normalidad(column, column_name):
+    #Sacamos ks statistic
     ks_statistic, p_value = stats.kstest(column, 'norm', args=(np.mean(column), np.std(column)))
 
     # Imprimir los resultados
     print(f"Estadístico de prueba (ks_statistic) = {ks_statistic:.20f}")
     print(f"p-value = {p_value:.20f}")
 
+    #Valor de (no me acuerdo que era jsjsjsj)
     alpha = 0.05
     if p_value < alpha:
         print(f"Se rechaza la hipótesis nula: los datos de '{column_name}' NO provienen de una distribución normal." + "\n")
@@ -42,8 +44,11 @@ def prueba_de_normalidad(column, column_name):
     plt.show()
         
 def frecuencias(column):
+    #Encontrar las veces que aparece cierto valor
     frecuencia = column.value_counts()
+    #El porcentaje de los valores
     porcentaje = column.value_counts(normalize=True) * 100
+    #Crear e imprimir la tabla
     tabla_frecuencia = pd.DataFrame({'Frecuencia': frecuencia, 'Porcentaje': porcentaje})
     print(tabla_frecuencia)
         
--- preguntas.py
+++ preguntas.py
@@ -8,9 +8,47 @@ import statsmodels.api as sm
 from tabulate import tabulate
 
 def top_10_budget_movies(df):
+    #Ordenar los datos de mayor a menor por presupuesto, enseñar solo los primeros 10
     top_10_budget = df.sort_values(by='budget', ascending=False).head(10)
     print(top_10_budget[['title', 'budget']])
     
 def top_10_revenue_movies(df):
+      #Ordenar los datos de mayor a menor por ingresos, enseñar solo los primeros 10
     top_10_revenue = df.sort_values(by='revenue', ascending=False).head(10)
     print(top_10_revenue[['title', 'revenue']])
+    
+def top_votes(df):
+      #Ordenar los datos de mayor a menor por cantidad de votos, enseñar solo el primero
+    top_votes = df.sort_values(by='voteCount', ascending=False).head(1)
+    print(top_votes[['title', 'voteCount']])
+
+def worst_movie(df):
+      #Ordenar los datos de menor a mayor por promedio en votos, enseñar solo el primero
+    worst_movie = df.sort_values(by='voteAvg', ascending=True).head(1)
+    print(worst_movie[['title', 'voteAvg']])
+    
+def movies_per_year(df):
+    #Convertir a formato date_time para poder extraer el año 
+    df['releaseDate'] = pd.to_datetime(df['releaseDate'], errors='coerce')
+    
+    #Estraer el año
+    df['year'] = df['releaseDate'].dt.year
+    
+    #Ordenar por indice y el valor es la cantidad que aparece
+    movies_per_year = df['year'].value_counts().sort_index()
+    
+    #Encontrar el año con más peliculas y la cantidad
+    anio_mas_peliculas = movies_per_year.idxmax()
+    cantidad_max = movies_per_year.max()
+
+    print(f"El año con más películas fue {anio_mas_peliculas} con {cantidad_max} películas.")
+    
+    #Generar el gráfico
+    plt.figure(figsize=(15, 6))
+
+    movies_per_year.plot(kind='bar', color='skyblue', edgecolor='black')
+    plt.xlabel("Año")
+    plt.ylabel("Cantidad de películas")
+    plt.title("Número de películas producidas por año")
+    
+    plt.show()
