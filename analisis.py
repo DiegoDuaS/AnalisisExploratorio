baab62736b0360851aa79ac53efab8c6ae8db3ac
MarchMol <marchmol98@gmail.com>
2025-02-02 20:02:06 -0600

    feat: preguntas g-i

--- analisis.py
+++ analisis.py
@@ -13,42 +13,21 @@ import statsmodels.stats.diagnostic as diag
 import statsmodels.api as sm
 
 def prueba_de_normalidad(column, column_name):
-    #Sacamos ks statistic
     ks_statistic, p_value = stats.kstest(column, 'norm', args=(np.mean(column), np.std(column)))
 
     # Imprimir los resultados
     print(f"Estadístico de prueba (ks_statistic) = {ks_statistic:.20f}")
     print(f"p-value = {p_value:.20f}")
 
-    #Valor de (no me acuerdo que era jsjsjsj)
     alpha = 0.05
     if p_value < alpha:
         print(f"Se rechaza la hipótesis nula: los datos de '{column_name}' NO provienen de una distribución normal." + "\n")
     else:
         print(f"No se rechaza la hipótesis nula: los datos de '{column_name}' parecen provenir de una distribución normal." + "\n")
         
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
-
-    # Graficar histograma
-    axes[0].hist(column)
-    axes[0].set_title(f'Histograma de {column_name}')
-    axes[0].set_xlabel(column_name)
-    axes[0].set_ylabel('Densidad')
-
-    # Graficar boxplot
-    axes[1].boxplot(column)
-    axes[1].set_title(f'Boxplot de {column_name}')
-    axes[1].set_ylabel(column_name)
-    # Mostrar gráficos
-    plt.tight_layout()
-    plt.show()
-        
 def frecuencias(column):
-    #Encontrar las veces que aparece cierto valor
     frecuencia = column.value_counts()
-    #El porcentaje de los valores
     porcentaje = column.value_counts(normalize=True) * 100
-    #Crear e imprimir la tabla
     tabla_frecuencia = pd.DataFrame({'Frecuencia': frecuencia, 'Porcentaje': porcentaje})
     print(tabla_frecuencia)
         
--- preguntas.py
+++ preguntas.py
@@ -52,3 +52,88 @@ def movies_per_year(df):
     plt.title("Número de películas producidas por año")
     
     plt.show()
+
+# --------- Inciso F ---------- #
+def recent_genre(df):
+    recent_movies = df.sort_values(by='releaseDate').head(20)
+    all_genres = recent_movies['genres']
+    recent_genres = all_genres.str.split('|').explode().value_counts()
+    print("Géneros en las 20 películas más recientes:\n",recent_genres)
+    
+def popular_genre(df):
+    # Cálculo de Génerp Principal
+    genres = df['genres']
+    top_genre = genres.str.split('|').explode().value_counts()
+    print("Género principal: \n",top_genre.head(1))
+    
+    # Histograma
+    histogram = plt.figure(figsize=(12, 5))
+    ax = histogram.add_subplot(1,1,1)
+    ax.bar(top_genre.index, top_genre.values, color="skyblue")
+    ax.set_xlabel("Género")
+    ax.set_ylabel("Conteo")
+    ax.set_title("Distribución de Géneros")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
+    
+def top_genre(df):
+    top_movies = df.sort_values(by='popularity').head(20)
+    top_genres = top_movies['genres']
+    genre_count = top_genres.str.split('|').explode().value_counts()
+    print("Géneros en 20 películas más populares: \n",genre_count)
+    
+# --------- Inciso G ---------- #
+def genre_revenue(df):
+    top_movies = df.sort_values(by="revenue", ascending=False).head(50)
+    tm_genres = top_movies['genres'].str.split('|').explode().drop_duplicates().to_numpy()
+    gen_revenue = pd.DataFrame(columns=['genre','revenueSum'])
+
+    for gen in tm_genres:
+        filtered = df[df['genres'].str.contains(gen, na=False)]
+        tem = pd.DataFrame({'genre':[gen],'revenueSum': [filtered['revenue'].sum()]})
+        gen_revenue = pd.concat([gen_revenue, tem],ignore_index=True)
+    gr_sorted = gen_revenue.sort_values(by='revenueSum', ascending=False)
+    print(gr_sorted)
+    
+# --------- Inciso H ---------- #
+def actor_amount_influence(df):
+    df1 = df[['revenue','actorsAmount']]
+    fig, ax= plt.subplots(nrows=1, ncols=2,figsize=(12, 5))
+    # Cantidad de Actores VS Ingresos
+    ax[0].scatter(df1['actorsAmount'], df1['revenue'], color='blue', marker='o', label='Data Points')
+    ax[0].set_xlabel('Cantidad de Actores')
+    ax[0].set_ylabel('Ingresos')
+    ax[0].set_title('Cantidad de Actores VS Ingresos de películas')
+
+    # Año VS Cantidad de actores
+    df2 = df[['releaseDate','actorsAmount']]
+    df2['releaseDate'] = pd.to_datetime(df2['releaseDate'],errors='coerce').dt.year
+    ax[1].scatter(df2['releaseDate'],df2['actorsAmount'])
+    ax[0].set_xlabel('Año de lanzamiento')
+    ax[0].set_ylabel('Cantidad de Actores')
+    ax[0].set_title('Año de lanzamiento VS Cantidad de Actores en películas')
+    plt.tight_layout()
+    plt.show()
+
+# --------- Inciso I ---------- #
+def gender_proportion(df):
+    dfi = df[['actorsAmount','castWomenAmount','castMenAmount','revenue','popularity']]
+    dfi = dfi[dfi['revenue']!=0]
+    dfi = dfi[dfi['castMenAmount']<500]
+    dfi['total'] = dfi['castWomenAmount'] +dfi['castMenAmount']
+    dfi['pWomen'] = dfi['castWomenAmount']/dfi['total']
+    dfi['pMen'] = dfi['castMenAmount']/dfi['total']
+    fig, ax= plt.subplots(nrows=1, ncols=2,figsize=(12, 5))
+
+    ax[0].scatter(dfi['pWomen'], dfi['revenue'], color="skyblue")
+    ax[0].set_xlabel("Proporción de Mujeres en el reparto")
+    ax[0].set_ylabel("Ingresos de la película")
+    ax[0].set_title("Proporción de Mujeres VS Ingresos")
+
+    ax[1].scatter(dfi['pWomen'], dfi['popularity'], color="skyblue")
+    ax[1].set_xlabel("Proporción de Mujeres en el reparto")
+    ax[1].set_ylabel("Popilaridad de la película")
+    ax[1].set_title("Proporción de Mujeres VS Popularidad")
+    plt.tight_layout()
+    plt.show()
