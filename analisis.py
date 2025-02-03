548a69adda3d6150a3c792837b8e26b986082b4e
DiegoDuaS <diego.slowing@gmail.com>
2025-02-02 23:28:58 -0600

    fix: reverted push that deleted the histograms

--- analisis.py
+++ analisis.py
@@ -24,6 +24,20 @@ def prueba_de_normalidad(column, column_name):
         print(f"Se rechaza la hipótesis nula: los datos de '{column_name}' NO provienen de una distribución normal." + "\n")
     else:
         print(f"No se rechaza la hipótesis nula: los datos de '{column_name}' parecen provenir de una distribución normal." + "\n")
+    
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+    # Graficar histograma
+    axes[0].hist(column)
+    axes[0].set_title(f'Histograma de {column_name}')
+    axes[0].set_xlabel(column_name)
+    axes[0].set_ylabel('Densidad')
+    # Graficar boxplot
+    axes[1].boxplot(column)
+    axes[1].set_title(f'Boxplot de {column_name}')
+    axes[1].set_ylabel(column_name)
+    # Mostrar gráficos
+    plt.tight_layout()
+    plt.show()
         
 def frecuencias(column):
     frecuencia = column.value_counts()
